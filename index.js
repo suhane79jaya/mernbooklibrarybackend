ebc459259e52dac884be048bb5cf087e7bb238ad
JayaSuhane <suhane79jaya@gmail.com>
2024-01-08 15:08:38 +0530

    final backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { mongoDBURL } from ".env";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
 import cors from "cors";
@@ -7,7 +7,6 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import { UserModel } from "./models/User.js";
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -92,7 +91,12 @@ app.get("/logout", (res, req) => {
   res.clearCookie("token", { path: "/" });
   res.status(200).send("User logout");
 });
-
+//step2 of deployment
+//const PORT = process.env.PORT || 5000;
+//step 3 of deployment
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
+// }
 mongoose
   .connect(mongoDBURL)
   .then(() => {
